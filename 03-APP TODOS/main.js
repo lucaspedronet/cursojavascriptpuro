0cd4ed19cafc2e6a4cfee26572b977fa7be62e7a
lucaspedronet <lucaspedrolbg@gmail.com>
2018-11-01 13:00:29 -0300

    Novas features com um modulo 04-APP-GITHUB e correções no modulo 03-APP TODOS e 02-MANIPULANDO-DOM

--- 03-APP TODOS/main.js
+++
@@ -1,8 +0,0 @@
-// JS ASSÍNCRONO 
-// AJAX é uma forma de solicitarmos requisições ao servidor sem ter a necessidade de atulizar a página.
-
-var xhr = new XMLHttpRequest(); //Atribuimos a xhr todas as funcionalidades da classe/função do AJAX, para podermos recuperar alguma requisição ao servidor.
-
-//por meio de xhr conseguimos invocar o método open() e passamos dois parametros o 1º é o método 'GET' e 2º a URL de github.
-xhr.open('GET', 'https://api.github.com/users/lucaspedronet')
-xhr.send(null)
