975a5c90d93cd386d7715932da7f022f996add63
lucaspedronet <lucaspedrolbg@gmail.com>
2018-11-07 09:10:07 -0300

    Features ES6

---
+++ 05-JAVASCRIPT-ES6/05-Parametro padrão/main.js
@@ -0,0 +1,3 @@
+const soma = (x=1, y=2) => x+y;
+console.log(soma());
+console.log(soma(5));
---
+++ 05-JAVASCRIPT-ES6/08-Template literals/main.js
@@ -0,0 +1,9 @@
+const usuario = {
+    nome: "Lucas Pedro",
+    idade: 26,
+    empresa: "Pulso"
+}
+
+//console.log("Meu nome é "+ usuario.nome+" e tenho "+usuario.idade+" anos de idade!");
+
+console.log(`Meu nome é ${usuario.nome} e tenho ${usuario.idade} anos de idade!`);
